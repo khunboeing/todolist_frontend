825b7cad1d7f7bf7af6e0f567cda907db30072d4
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-03-07 08:33:52 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Todolist from "./todolist/Todolist.jsx";
+
+function App() {
+  return (
+    <>
+      <Todolist />
+    </>
+  );
+}
+
+export default App;
